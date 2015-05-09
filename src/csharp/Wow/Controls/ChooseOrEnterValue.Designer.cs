18d97e0dbfc7f216a348168bf4046797431eed29
strayptr <strayptr@gmail.com>
2015-05-09 01:51:49 -0500

    + Wow.Controls

---
+++ src/csharp/Wow/Controls/ChooseOrEnterValue.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Controls
+{
+    partial class ChooseOrEnterValue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbValue = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbValue
+            // 
+            this.tbValue.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbValue.Location = new System.Drawing.Point(0, 0);
+            this.tbValue.Name = "tbValue";
+            this.tbValue.Size = new System.Drawing.Size(159, 20);
+            this.tbValue.TabIndex = 0;
+            // 
+            // ChooseOrEnterValue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tbValue);
+            this.Name = "ChooseOrEnterValue";
+            this.Size = new System.Drawing.Size(159, 24);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbValue;
+    }
+}
---
+++ src/csharp/Wow/Controls/ChooseOrEnterValue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Controls
+{
+    public partial class ChooseOrEnterValue: UserControl
+    {
+        //-----------------------------------------------------------------------------
+        public ChooseOrEnterValue()
+        {
+            InitializeComponent();
+        }
+
+        //-----------------------------------------------------------------------------
+        public delegate void ValueChangedHandler(object sender);
+        public event ValueChangedHandler OnValueChanged;
+
+        //=============================================================================
+        // Properties
+        //=============================================================================
+
+        //-----------------------------------------------------------------------------
+        public string Value
+        {
+            get { return tbValue.Text; }
+            set
+            {
+                if (value != tbValue.Text)
+                {
+                    tbValue.Text = value;
+                    OnValueChanged(this);
+                }
+            }
+        }
+    }
+}
