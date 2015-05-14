7bafa856629eda87b1f6ec1d0827616b660fe9a5
strayptr <strayptr@gmail.com>
2015-05-14 06:10:35 -0500

    OpenGL experimentation.

---
+++ src/csharp/Wow/Controls/Graphics/GLView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace Controls.Graphics
+{
+    public partial class GLView : GLControl
+    {
+        public GLView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            if (!this.DesignMode)
+            {
+                MakeCurrent();
+                GL.ClearColor(Color.Black);
+                GL.Clear(ClearBufferMask.ColorBufferBit);
+                SwapBuffers();
+            }
+        }
+    }
+}
